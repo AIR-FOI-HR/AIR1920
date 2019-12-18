2dd3e68fce31c5fb5d4f5b4005e8577b1224bb3f
Enis <enovak1@foi.hr>
2019-12-18 14:33:48 +0100

    Prebaceni dohvat podataka preko DataLoadera

--- app/src/main/java/com/example/culturearound/MainActivity.java
+++ app/src/main/java/com/example/culturearound/MainActivity.java
@@ -8,17 +8,23 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.example.core.DataLoadedListener;
+import com.example.core.DataLoader;
 import com.example.database.Entities.Korisnik;
+import com.example.database.Entities.Lokacija;
+import com.example.database.Entities.Znamenitost;
 import com.example.database.MyDatabase;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import Data.MockData;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import loaders.DbDataLoader;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DataLoadedListener {
 
     @BindView(R.id.korisnici_list)
     ListView mListView;
@@ -33,15 +39,20 @@ public class MainActivity extends AppCompatActivity {
         database = MyDatabase.getInstance(this);
 
         ButterKnife.bind(this);
-        mockData();
+        //mockData();
     }
 
     @OnClick(R.id.test_button)
     public void buttonClicked(View view)
     {
+        /*
         final List<String> listItems = database.getDAO().getKorisnickoIme();
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems);
         mListView.setAdapter(adapter);
+        */
+        DataLoader dataLoader = new DbDataLoader();
+        //DataLoader dataLoader = new WsDataLoader();
+        dataLoader.loadData(this);
     }
 
 
@@ -55,5 +66,17 @@ public class MainActivity extends AppCompatActivity {
         }else {
             MockData.writeData(this);
         }
+    }
+
+    @Override
+    public void onDataLoaded(List<Korisnik> korisnici, List<Znamenitost> znamenitosti, List<Lokacija> lokacije) {
+        final List<String> listItems = new ArrayList<>();
+
+        for (Korisnik k: korisnici) {
+            listItems.add(k.getKorisnicko_ime());
+        }
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems.toArray());
+        mListView.setAdapter(adapter);
     }
 }
