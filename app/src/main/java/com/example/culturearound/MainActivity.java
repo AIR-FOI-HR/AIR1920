ed8d0c57c7e8be7e1806e47825f09e5b646a40aa
Luka Čosić <cosic.luka1@gmail.com>
2019-12-19 10:54:54 +0100

    Brisanje nepotrebnih stvari
    Brisanje nepotrebnih stvari iz MainActivitija koje su služile za ranije testiranje.
    Isto tako obrisani gumb i listview sa layouta activity_main.

--- app/src/main/java/com/example/culturearound/MainActivity.java
+++ app/src/main/java/com/example/culturearound/MainActivity.java
@@ -26,9 +26,6 @@ import loaders.DbDataLoader;
 
 public class MainActivity extends AppCompatActivity implements DataLoadedListener {
 
-    @BindView(R.id.korisnici_list)
-    ListView mListView;
-
     public static MyDatabase database;
 
     @Override
@@ -40,19 +37,6 @@ public class MainActivity extends AppCompatActivity implements DataLoadedListene
 
         ButterKnife.bind(this);
         mockData();
-    }
-
-    @OnClick(R.id.test_button)
-    public void buttonClicked(View view)
-    {
-        /*
-        final List<String> listItems = database.getDAO().getKorisnickoIme();
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems);
-        mListView.setAdapter(adapter);
-        */
-        DataLoader dataLoader = new DbDataLoader();
-        //DataLoader dataLoader = new WsDataLoader();
-        dataLoader.loadData(this);
     }
 
 
@@ -80,16 +64,7 @@ public class MainActivity extends AppCompatActivity implements DataLoadedListene
 
     @Override
     public void onDataLoaded(List<Korisnik> korisnici, List<Znamenitost> znamenitosti, List<Lokacija> lokacije) {
-        final List<String> listItems = new ArrayList<>();
 
-        for (Korisnik k: korisnici) {
-            listItems.add(k.getKorisnicko_ime());
-        }
-        for(Znamenitost z : znamenitosti){
-            listItems.add((z.getNaziv()));
-        }
-
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems.toArray());
-        mListView.setAdapter(adapter);
+        //ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems.toArray());
     }
 }
