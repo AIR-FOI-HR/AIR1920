8d8a62232981cb17ca9c06b61594eaeb2828dde6
Luka Čosić <cosic.luka1@gmail.com>
2019-12-19 11:15:17 +0100

    Stvoren ZnamenitostRecyclerAdapter
    koji služi da sve listu znamenitosti stavi u holder.

---
+++ app/src/main/java/com/example/culturearound/PretrazivanjeZnamenitosti/recyclerview/ZnamenitostRecyclerAdapter.java
@@ -0,0 +1,44 @@
+package com.example.culturearound.PretrazivanjeZnamenitosti.recyclerview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.culturearound.MainActivity;
+import com.example.culturearound.R;
+import com.example.database.Entities.Znamenitost;
+
+import java.util.List;
+
+public class ZnamenitostRecyclerAdapter extends RecyclerView.Adapter<ZnamenitostViewHolder>{
+    private Context context;
+    List<Znamenitost> znamenitosti;
+
+
+    public ZnamenitostRecyclerAdapter(Context context, @NonNull List<Znamenitost> znamenitosti) {
+        this.context = context;
+        this.znamenitosti = znamenitosti;
+    }
+
+    @NonNull
+    @Override
+    public ZnamenitostViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View znamenitostView = LayoutInflater.from(parent.getContext()).inflate(R.layout.znamenitost_list_item, parent, false);
+        return new ZnamenitostViewHolder(znamenitostView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ZnamenitostViewHolder holder, int position) {
+        holder.bindToData(znamenitosti.get(position));
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return znamenitosti.size();
+    }
+}
