b135dc4d5867678446e131fa5ca30f2f442cd337
Luka Čosić <cosic.luka1@gmail.com>
2020-01-30 02:32:56 +0100

    Prikaz podataka znamenitosti
    Dodan dependecy tako da spomenik_znamenitost može komunicirati s app i database moduleom.
    Modul spomenik_znamenitost ima sve potrebne dependecyje za rad.
    LokacijaHelper od sada dohvaća lokaciju prema ID-u.
    ZnamDetailsSpomenikActivity šalje zahtjev helperu za znamenitost i lokaciju te osluškuje odgovor. Kada dobije odgovor prikazuje podatke na formi.

--- spomenik_znamenitost/src/main/java/com/example/spomenik_znamenitost/ZnamDetailsSpomenikActivity.java
+++ spomenik_znamenitost/src/main/java/com/example/spomenik_znamenitost/ZnamDetailsSpomenikActivity.java
@@ -1,18 +1,41 @@
 package com.example.spomenik_znamenitost;
 
+
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.database.EntitiesFirebase.Lokacija;
+import com.example.database.EntitiesFirebase.Znamenitost;
+import com.example.database.Listeners.LokacijaListener;
+import com.example.database.Listeners.ZnamenitostListener;
+import com.example.database.LokacijaHelper;
+import com.example.database.ZnamenitostiHelper;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class ZnamDetailsSpomenikActivity extends AppCompatActivity {
+import com.squareup.picasso.Picasso;
 
+import java.util.List;
+
+public class ZnamDetailsSpomenikActivity extends AppCompatActivity implements ZnamenitostListener, LokacijaListener {
+    private ZnamenitostiHelper znamenitostiHelper;
+    private LokacijaHelper lokacijaHelper;
+
+    ImageView imgNaslovnaSlika;
+    TextView txtAdresaZnamenitosti;
+    TextView txtLokacijaZnamenitosti;
+    TextView txtOpisZnamenitosti;
+    RecyclerView recyclerViewGalerija;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,24 +48,68 @@ public class ZnamDetailsSpomenikActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Dodaj u favourite", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+
+        znamenitostiHelper = new ZnamenitostiHelper((Context) this, this);
+        lokacijaHelper = new LokacijaHelper((Context) this, this);
+
+        imgNaslovnaSlika = (ImageView) findViewById(R.id.imgNaslovnaSlika);
+        txtAdresaZnamenitosti = (TextView) findViewById(R.id.txtAdresaZnamenitosti);
+        txtLokacijaZnamenitosti = (TextView) findViewById(R.id.txtLokacijaZnamenitosti);
+        txtOpisZnamenitosti = (TextView) findViewById(R.id.txtOpisZnamenitosti);
+        recyclerViewGalerija = (RecyclerView) findViewById(R.id.recyclerViewGalerija);
     }
     @Override
     protected void onResume(){
         super.onResume();
 
-
         Intent intent = getIntent();
         int znamId = intent.getIntExtra("id_znamenitost", -1);
 
-
         if(znamId != -1){
-            //znamenitostiHelper.dohvatiZnamenitostPremaId(znamId);
-            setTitle("Naziv");
+            znamenitostiHelper.dohvatiZnamenitostPremaId(znamId);
         }
 
+    }
+
+    private void prikaziPodatkeZnamenitosti(Znamenitost znamenitost){
+        setTitle(znamenitost.getNaziv());
+        Picasso.with(this)
+                .load(znamenitost.getLokacijaSlike())
+                .into(imgNaslovnaSlika);
+        txtAdresaZnamenitosti.setText(znamenitost.getAdresa());
+
+        lokacijaHelper.dohvatiZnamenitostPremaId(znamenitost.getIdLokacija());
+
+        txtOpisZnamenitosti.setText(znamenitost.getOpis());
+
+        //recyclerview komentari
+    }
+
+    private void prikaziLokaciju(Lokacija lokacija) {
+        txtLokacijaZnamenitosti.setText(lokacija.getNaziv());
+    }
+
+    @Override
+    public void onLoadZnamenitostSucess(String message, List<Znamenitost> listaZnamenitosti) {
+        prikaziPodatkeZnamenitosti(listaZnamenitosti.get(0));
+    }
+
+    @Override
+    public void onLoadZnamenitostFail(String message) {
+        Log.d("SpomenikTag", "message");
+    }
+
+    @Override
+    public void onLoadLokacijaSucess(String message, List<Lokacija> listaLokacija) {
+        prikaziLokaciju(listaLokacija.get(0));
+    }
+
+    @Override
+    public void onLoadLokacijaFail(String message) {
+        Log.d("SpomenikTag", "message");
     }
 }
