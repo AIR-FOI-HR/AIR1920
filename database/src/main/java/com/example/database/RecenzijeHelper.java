a761188fd80727218f83b3ee05c2ee5d475bb519
Luka Čosić <cosic.luka1@gmail.com>
2020-02-03 00:49:23 +0100

    Podešen rad recenzija s bazom
    Podešen rad recenzija s bazom na način da se u realnom vremenu ažuriraju vrijednosti koje se prikazuju. Također izbrisana metoda koja se nigdje ne koristi i nigdje se neće koristiti (dohvati sve recenzije).

--- database/src/main/java/com/example/database/RecenzijeHelper.java
+++ database/src/main/java/com/example/database/RecenzijeHelper.java
@@ -28,7 +28,6 @@ import java.util.Arrays;
 public class RecenzijeHelper extends FirebaseHelper {
     private RecenzijaListener recenzijaListener;
 
-
     public RecenzijeHelper (Context context, RecenzijaListener recenzijaListener){
         mAuth = FirebaseAuth.getInstance();
         mContext = context;
@@ -38,39 +37,7 @@ public class RecenzijeHelper extends FirebaseHelper {
 
     }
 
-
-
     public double ukupno;
-
-
-
-    public void dohvatiSveRecenzije(){
-        if (!provjeriDostupnostMreze()) return;
-        mQuery = mDatabase.child("komentar");
-        if (provjeriDostupnostMreze()){
-            mQuery.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    Log.d("Anja", "Dobavljanje..");
-                    List<Komentar> listaRecenzija = new ArrayList<>();
-                    for (DataSnapshot temp : dataSnapshot.getChildren()){
-                        Komentar komentar = new Komentar();
-                        komentar = temp.getValue(Komentar.class);
-                        komentar.setIdKomentar(Integer.parseInt(temp.getKey()));
-                        listaRecenzija.add(komentar);
-                    }
-                    Log.d("Anja", "Dobavljanje DONE");
-                    recenzijaListener.onLoadRecenzijaSucess("Uspješno dohvaćanje - listener", listaRecenzija);
-                }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError databaseError) {
-                    recenzijaListener.onLoadRecenzijaFail("Neuspješno dohvaćanje - listener");
-                }
-            });
-        }
-    }
-
     double zbrOcjena=0;
     double brRec=0;
     double vrijednost;
@@ -78,25 +45,19 @@ public class RecenzijeHelper extends FirebaseHelper {
     public void dohvatiRecenzijePremaId(final int idZnamenitost){
         if (!provjeriDostupnostMreze()) return;
         mQuery = mDatabase.child("komentar").child(Integer.toString(idZnamenitost));
-        mQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+        mQuery.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 List<Komentar> listaRecenzija = new ArrayList<>();
                 for (DataSnapshot temp : dataSnapshot.getChildren()){
-                    Log.d("Anja1", "Dosao do tud ");
                     Komentar komentar = new Komentar();
                     komentar = temp.getValue(Komentar.class);
                     komentar.setUid(temp.getKey());
                     komentar.setIdZnamenitost(idZnamenitost);
-                    Log.d("Anja", "Uid anja: " + komentar.getUid());
-                    Log.d("Anja", "Idznam anja: " + komentar.getIdZnamenitost());
-                    Log.d("Anja", "opis anja: " + komentar.getOpis());
                     zbrOcjena+=komentar.getOcjena();
                     brRec+=1;
                     listaRecenzija.add(komentar);
                 }
-                Log.d("Anja1", "Zbroj: " + zbrOcjena);
-                Log.d("Anja1", "Broj:"+brRec);
                 if (zbrOcjena!=0){
                     vrijednost=zbrOcjena/brRec;
                     Double toBeTruncated = new Double(vrijednost);
@@ -108,11 +69,8 @@ public class RecenzijeHelper extends FirebaseHelper {
                 else{
                     ukupno=0;
                 }
-
-                Log.d("Anja2", "BrojSve:"+ukupno);
                 recenzijaListener.onLoadRecenzijaSucess("Uspješno dohvaćanje - listener", listaRecenzija);
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
                 recenzijaListener.onLoadRecenzijaFail("Neuspješno dohvaćanje - listener");
@@ -124,28 +82,22 @@ public class RecenzijeHelper extends FirebaseHelper {
     public void zapisiRecenziju(final String opis, final int ocjena, final int idZnamenitost){
         if (!provjeriDostupnostMreze()) return;
         final DatabaseReference aKomentar = mDatabase.child("komentar");
-        Log.d("Anja3:","ovdje sam");
-        aKomentar.addListenerForSingleValueEvent(new ValueEventListener() {
+        aKomentar.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 if(dataSnapshot.child(Integer.toString(idZnamenitost)).hasChild(mAuth.getUid())){
                     Toast.makeText(mContext, "Recenzija već postavljena", Toast.LENGTH_LONG).show();
-
                 }
                 else{
                     Komentar noviKomentar = new Komentar(7,opis,ocjena, mAuth.getUid(),idZnamenitost,0,0);
                     aKomentar.child(Integer.toString(idZnamenitost)).child(mAuth.getUid()).setValue(noviKomentar);
                     Toast.makeText(mContext, "Uspješna recenzija", Toast.LENGTH_LONG).show();
-
                 }
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-
             }
         });
-
     }
 
     public void provjeriOcjenuKomentara(String otherUId, final int idZnamenitosti){
@@ -155,14 +107,12 @@ public class RecenzijeHelper extends FirebaseHelper {
             mQuery.addValueEventListener(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    Log.d("PromjenaTag", "onDataChange");
                     if (dataSnapshot.hasChild(Integer.toString(idZnamenitosti))){
                         String mojaOcjenaKomentara =
                                 (String) dataSnapshot.child(Integer.toString(idZnamenitosti)).child("vote").getValue();
                         recenzijaListener.onOcjenaKomentaraSucess("Dohvaćena ocjena komentara!", mojaOcjenaKomentara);
                     }
                 }
-
                 @Override
                 public void onCancelled(@NonNull DatabaseError databaseError) {
                     recenzijaListener.onLoadRecenzijaFail("Nije dohvaćena ocjena komentara!");
@@ -172,13 +122,10 @@ public class RecenzijeHelper extends FirebaseHelper {
     }
 
     public void postaviOcjenu(String otherUId, int idZnamenitosti, String ocjena){
-        Log.d("NjAnja", "Postavlja ocjenu... " + ocjena + " sa idZnam " + Integer.toString(idZnamenitosti) + " otherUID " + otherUId);
         mDatabase.child("Like").child(mAuth.getUid()).child(otherUId).child(Integer.toString(idZnamenitosti)).child("vote").setValue(ocjena);
         mQuery = mDatabase.child("Komentar").child(Integer.toString(idZnamenitosti)).child(otherUId);
         if (ocjena.equals("Like")){
-            Log.d("NjAnja","PromijeniVrijednost ocjene...");
             promijeniVrijednostOcjene(otherUId, idZnamenitosti, "brojUp", "povecaj");
-            Log.d("NjAnja","PromijeniVrijednost ocjene...Done");
         }
         else{
             promijeniVrijednostOcjene(otherUId, idZnamenitosti, "brojDown", "povecaj");
@@ -194,28 +141,22 @@ public class RecenzijeHelper extends FirebaseHelper {
                 int broj = Integer.parseInt(dataSnapshot.getValue().toString());
                 if (radnja.equals("povecaj")) broj++; //plus 1 lajk
                 else broj--;
-                Log.d("NjAnja", "!!Spremanje broja...");
                 mDatabase.child("komentar")
                         .child(Integer.toString(idZnamenitosti))
                         .child(otherUId)
                         .child(smjer).setValue(broj);
-                Log.d("NjAnja", "!!Spremanje broja...DONE");
             }
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.d("FirebaseTag","aAnja");
             }
         });
     }
 
     public void obrisiOcjenu(String otherUId, int idZnamenitosti, String ocjena){
         mDatabase.child("Like").child(mAuth.getUid()).child(otherUId).child(Integer.toString(idZnamenitosti)).child("vote").removeValue();
-        Log.d("kuda","dosao tu");
         mQuery = mDatabase.child("Komentar").child(Integer.toString(idZnamenitosti)).child(otherUId);
         if (ocjena.equals("Like")){
-            Log.d("NjAnja","PromijeniVrijednost ocjene...");
             promijeniVrijednostOcjene(otherUId, idZnamenitosti, "brojUp", "smanji");
-            Log.d("NjAnja","PromijeniVrijednost ocjene...Done");
         }
         else{
             promijeniVrijednostOcjene(otherUId, idZnamenitosti, "brojDown", "smanji");
